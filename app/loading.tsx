376a0cc6731801f35bc7e1fea3698d2e8aee4e96
Sean-Patrick <rowan.seanp@gmail.com>
2024-03-28 14:12:58 -0400

    SETUP!: Reinstalled base next js, installed Tremor UI dependencies

--- app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Title } from '@tremor/react';
-
-export default function Loading() {
-  return (
-    <main className="p-4 md:p-10 mx-auto max-w-7xl">
-      <Title>Loading...</Title>
-    </main>
-  );
-}
