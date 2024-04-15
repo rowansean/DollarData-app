288e33dbf0a3243cd2bdaa4783f0b07119b4a5b3
Sean-Patrick <rowan.seanp@gmail.com>
2024-04-15 07:56:01 -0400

    fix: fixed key on main graph

--- src/components/NetWorthOverTime.tsx
+++ src/components/NetWorthOverTime.tsx
@@ -1,81 +1,66 @@
 import { AreaChart } from "@tremor/react";
 
 const chartdata = [
-  {
-    date: "Jan 22",
-    SemiAnalysis: 2890,
-    "The Pragmatic Engineer": 2338,
-  },
-  {
-    date: "Feb 22",
-    SemiAnalysis: 2756,
-    "The Pragmatic Engineer": 2103,
-  },
-  {
-    date: "Mar 22",
-    SemiAnalysis: 3322,
-    "The Pragmatic Engineer": 2194,
-  },
-  {
-    date: "Apr 22",
-    SemiAnalysis: 3470,
-    "The Pragmatic Engineer": 2108,
-  },
-  {
-    date: "May 22",
-    SemiAnalysis: 3475,
-    "The Pragmatic Engineer": 1812,
-  },
-  {
-    date: "Jun 22",
-    SemiAnalysis: 3129,
-    "The Pragmatic Engineer": 1726,
-  },
-  {
-    date: "Jul 22",
-    SemiAnalysis: 3490,
-    "The Pragmatic Engineer": 1982,
-  },
-  {
-    date: "Aug 22",
-    SemiAnalysis: 2903,
-    "The Pragmatic Engineer": 2012,
-  },
-  {
-    date: "Sep 22",
-    SemiAnalysis: 2643,
-    "The Pragmatic Engineer": 2342,
-  },
-  {
-    date: "Oct 22",
-    SemiAnalysis: 2837,
-    "The Pragmatic Engineer": 2473,
-  },
-  {
-    date: "Nov 22",
-    SemiAnalysis: 2954,
-    "The Pragmatic Engineer": 3848,
-  },
-  {
-    date: "Dec 22",
-    SemiAnalysis: 3239,
-    "The Pragmatic Engineer": 3736,
-  },
+    {
+        date: "Jan 22",
+        'Net Worth': 2890,
+    },
+    {
+        date: "Feb 22",
+        'Net Worth': 2756,
+    },
+    {
+        date: "Mar 22",
+        'Net Worth': 3322,
+    },
+    {
+        date: "Apr 22",
+        'Net Worth': 3470,
+    },
+    {
+        date: "May 22",
+        'Net Worth': 3475,
+    },
+    {
+        date: "Jun 22",
+        'Net Worth': 3129,
+    },
+    {
+        date: "Jul 22",
+        'Net Worth': 3490,
+    },
+    {
+        date: "Aug 22",
+        'Net Worth': 2903,
+    },
+    {
+        date: "Sep 22",
+        'Net Worth': 2643,
+    },
+    {
+        date: "Oct 22",
+        'Net Worth': 2837,
+    },
+    {
+        date: "Nov 22",
+        'Net Worth': 2954,
+    },
+    {
+        date: "Dec 22",
+        'Net Worth': 3239,
+    },
 ];
 
 
 export function NetWorthOverTime() {
   return (
     <>
-      <p className="text-tremor-metric text-tremor-content-strong dark:text-dark-tremor-content-strong font-semibold">
-        $34,567
-      </p>
       <AreaChart
         className="mt-4 h-72"
         data={chartdata}
         index="date"
         yAxisWidth={65}
-        categories={["SemiAnalysis", "The Pragmatic Engineer"]}
+        categories={["Net Worth"]}
         colors={["indigo", "cyan"]}
       />
     </>
