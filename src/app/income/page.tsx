25375ff8b50c53c2d298c7d8365060432f6602eb
jadenPete <jadenpete@github.com>
2024-04-15 21:22:35 -0400

    Added an income page

--- src/app/income/page.tsx
+++ src/app/income/page.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+"use client";
 
-export default function IncomePage() {
-  return <div>IncomePage</div>;
+import React from "react";
+import { CashFlowType, MonthlyCashFlow } from "@/lib/types";
+import { CashFlowPage } from "@/components/CashFlowPage";
+
+export default function ExpensesPage() {
+	const [monthlyIncome, setMonthlyIncome] = React.useState<MonthlyCashFlow>(() => {
+		const result = new Map();
+
+		result.set(Date.now(), [
+			{
+				name: "Salary",
+				amountCents: 400000
+			}
+		]);
+
+		return result;
+	});
+
+	return <CashFlowPage
+		monthlyCashFlow={monthlyIncome}
+		type={CashFlowType.Income}
+		onChange={newMonthlyIncome => setMonthlyIncome(newMonthlyIncome)}/>;
 }
