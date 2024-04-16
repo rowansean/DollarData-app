9c5ee404b21b9c57b5f05b7a75b46e2e971c5d57
jadenPete <jadenpete@github.com>
2024-04-15 23:33:17 -0400

    Show real expense and income values in every other component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,42 @@ import MonthlySavingsProgress from "@/components/MonthlySavingsProgress";
 import NetWorthMonthChange from "@/components/NetWorthMonthChange";
 import { NetWorthOverTime } from "@/components/NetWorthOverTime";
 import { useMonthlyExpenses, useMonthlyIncome } from "@/hooks/useMonthlyCashFlow";
+import { MonthlyCashFlow, currentMonth, formatCurrency } from "@/lib/types";
+
+function currentMonthFormatted(): string {
+  return new Date(currentMonth).toLocaleString("en-US", {
+    month: "long"
+  });
+}
+
+function sumMonthlyCashFlow(monthlyCashFlow: MonthlyCashFlow): number {
+  return Array
+    .from(monthlyCashFlow.values())
+    .flat()
+    .map(item => item.amountCents)
+    .reduce((previous, current) => previous + current, 0);
+}
+
+function todayFormatted(): string {
+  return new Date().toLocaleString("en-US", {
+    dateStyle: "short"
+  });
+}
 
 export default function Home() {
 	const [monthlyExpenses, _1] = useMonthlyExpenses();
   const [monthlyIncome, _2] = useMonthlyIncome();
+  const currentMonthExpenses = (monthlyExpenses.get(currentMonth.getTime()) ?? [])
+    .reduce((previous, current) => previous + current.amountCents, 0);
+
+  const currentMonthIncome = (monthlyIncome.get(currentMonth.getTime()) ?? [])
+    .reduce((previous, current) => previous + current.amountCents, 0);
+
+  const currentMonthNetCashFlow = currentMonthIncome - currentMonthExpenses;
+  const currentMonthNetCashFlowTarget = currentMonthIncome * 0.2;
+  const netWorth = sumMonthlyCashFlow(monthlyIncome) - sumMonthlyCashFlow(monthlyExpenses);
+  const netWorthChange = currentMonthNetCashFlow / (netWorth - currentMonthNetCashFlow) * 100;
+  const netWorthChangeFormatted = `${netWorthChange >= 0 ? "+" : ""}${netWorthChange.toFixed(1)}%`;
 
   return (
     <main className="w-screen min-h-screen flex flex-col gap-5 p-5 md:p-10">
@@ -19,21 +51,20 @@ export default function Home() {
           <BadgeDelta
             deltaType="moderateIncrease"
             isIncreasePositive={true}
-            size="xs"
-          >
-            +9.3%
+            size="xs">
+            {netWorthChangeFormatted}
           </BadgeDelta>
         </div>
-        <h1 className="text-6xl font-bold">$74,390.13</h1>
+        <h1 className="text-6xl font-bold">{formatCurrency(netWorth)}</h1>
         <p className="text-sm font-light">
-          your total assets minus liabilities as of 04/15/2000
+          your total assets minus liabilities as of {todayFormatted()}
         </p>
       </div>
       <div className="dashboard-grid grid grid-cols-1 md:grid-cols-3 gap-5">
         <Card className="flex justify-between items-center">
           <div className="h-fit text-left">
             <p className="font-light text-sm">This Month&apos;s Change</p>
-            <p className="font-medium text-lg">April</p>
+            <p className="font-medium text-lg">{currentMonthFormatted()}</p>
           </div>
 
           <NetWorthMonthChange
@@ -42,17 +73,21 @@ export default function Home() {
             monthlyIncome={monthlyIncome}/>
 
           <div>
-            <p>$196.26</p>
-            <p>+9.3%</p>
+            <p>{formatCurrency(currentMonthNetCashFlow)}</p>
+            <p>{netWorthChangeFormatted}</p>
           </div>
         </Card>
 
         <Card className="flex justify-between items-center">
           <div className="h-fit text-left">
             <p className="font-light text-sm">Your Monthly Savings Goal</p>
-            <p className="font-medium text-lg">$45 / $340</p>
+            <p className="font-medium text-lg">
+              {formatCurrency(currentMonthNetCashFlow)} / {formatCurrency(currentMonthNetCashFlowTarget)}
+            </p>
           </div>
-          <MonthlySavingsProgress className="" />
+          <MonthlySavingsProgress
+            actual={currentMonthNetCashFlow}
+            target={currentMonthNetCashFlowTarget}/>
         </Card>
 
         <Card className="flex flex-col gap-2">
--- src/components/MonthlySavingsProgress.tsx
+++ src/components/MonthlySavingsProgress.tsx
@@ -1,17 +1,12 @@
-"use client";
-
 import { ProgressCircle } from "@tremor/react";
 
-export default function MonthlySavingsProgress({
-  className,
-}: {
-  className: string;
+export default function MonthlySavingsProgress({ actual, target }: {
+  actual: number
+  target: number
 }) {
-  return (
-    <div className={`${className}`}>
-      <ProgressCircle value={75} size="md">
-        <span className="text-xs font-medium text-slate-700">75%</span>
-      </ProgressCircle>
-    </div>
-  );
+  const percentage = Math.round(Math.min(actual / target, 1) * 100);
+
+  return <ProgressCircle value={percentage} size="md">
+    <span className="text-xs font-medium text-slate-700">{percentage}%</span>
+  </ProgressCircle>;
 }
